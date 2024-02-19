5274349102936a977674fba89b4de713641a0e7e
trias <triasnugra97@gmail.com>
2024-02-19 14:08:38 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+const scale = 20;
+const rows = canvas.height / scale;
+const columns = canvas.width / scale;
+
+var snake;
+
+(function setup() {
+    snake = new Snake();
+    fruit = new Fruit();
+    fruit.pickLocation();
+
+    window.setInterval(() => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        fruit.draw();
+        snake.update();
+        snake.draw();
+
+        if (snake.eat(fruit)) {
+            fruit.pickLocation();
+        }
+
+        snake.checkCollision();
+        document.querySelector('.score')
+            .innerText = snake.total;
+
+    }, 250);
+}());
+
+window.addEventListener('keydown', evt => {
+    const direction = evt.key.replace('Arrow', '');
+    snake.changeDirection(direction);
+});
